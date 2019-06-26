a0696fab867c4d404539ccfab17b0dd4dc8f5ffe
qichunlin <737795279@qq.com>
2019-06-26 13:32:23 +0800

    [update] springboot Test config

--- springboot-config/src/main/java/com/legend/springboot/config/MyAppConfig.java
+++ springboot-config/src/main/java/com/legend/springboot/config/MyAppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * 在配置文件中用<bean><bean/>标签添加组件
  *
  */
-//@Configuration
+@Configuration
 public class MyAppConfig {
 
     //将方法的返回值添加到容器中；容器中这个组件默认的id就是方法名
