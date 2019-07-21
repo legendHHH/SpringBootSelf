c13e0bd2c5af134dba6113303f007ed5d776208c
qichunlin <737795279@qq.com>
2019-07-21 21:56:35 +0800

    [ADD] springboot-data-jdbc project, springboot试用jdbc操作数据

---
+++ springboot-data-jdbc/src/main/java/com/legend/springboot/config/DruidConfig.java
@@ -0,0 +1,64 @@
+package com.legend.springboot.config;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Druid配置
+ * @author legend
+ */
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+
+    //配置Druid监控
+    //1.配置一个管理后台的servlet
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        //拦截 /druid  的所有请求
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid");
+        Map<String, String> initParameters = new HashMap<>();
+        //设置相关配置
+        initParameters.put("loginUsername","admin");
+        initParameters.put("loginPassword","123456");
+        initParameters.put("allow","");//默认就是允许所有访问
+        //initParameters.put("deny","192.168.12.3");
+        bean.setInitParameters(initParameters);
+        return bean;
+    }
+
+    //2.配置一个监控的filter
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        //创建过滤器
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+
+        //排除拦截的静态文件
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("exclusions","*.js,*.css,/druid/*");
+
+        //忽略过滤的形式
+        bean.setInitParameters(initParams);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return  bean;
+    }
+}
