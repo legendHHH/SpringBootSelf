53bd8a84e419a733f5f0586bea77c2050cee96cb
qichunlin <737795279@qq.com>
2019-10-25 18:14:05 +0800

    springboot2.0.6 版本回顾配置类

---
+++ springboot-test/src/main/java/com/legend/demo/test2/config/JdbcProperties.java
@@ -0,0 +1,48 @@
+package com.legend.demo.test2.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties(prefix = "jdbc")
+public class JdbcProperties {
+
+    /**
+     * 属性名必须要和application配置文件中的属性名相同
+     */
+    private String driverClassName;
+    private String url;
+    private String userName;
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- springboot-test/src/main/java/com/legend/demo/test2/controller/HelloController.java
+++ springboot-test/src/main/java/com/legend/demo/test2/controller/HelloController.java
@@ -1,4 +1,26 @@
 package com.legend.demo.test2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
+
+    //@Autowired
+    private DataSource dataSource;
+
+    //通过构造方法注入  @Autowired
+    public HelloController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @RequestMapping("/show")
+    public String helloW() {
+        System.out.println(dataSource);
+        return "HAND Hello";
+    }
 }
