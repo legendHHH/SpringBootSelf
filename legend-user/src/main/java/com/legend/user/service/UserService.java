ae11bdd0048020a8a1ed98160b4729ff45c740ff
qichunlin <737795279@qq.com>
2019-10-27 16:18:54 +0800

    springboot实战回顾--legend-user项目

---
+++ legend-user/src/main/java/com/legend/user/service/UserService.java
@@ -0,0 +1,23 @@
+package com.legend.user.service;
+
+import com.legend.user.mapper.UserMapper;
+import com.legend.user.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User queryById(Long id){
+        return this.userMapper.selectByPrimaryKey(id);
+    }
+
+    @Transactional
+    public void deleteById(Long id){
+        this.userMapper.deleteByPrimaryKey(id);
+    }
+}
