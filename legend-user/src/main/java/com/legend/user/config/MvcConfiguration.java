e4482e53e21dbf86f7c6108fa4c23861761dd54b
qichunlin <737795279@qq.com>
2019-10-28 19:12:50 +0800

    【UPDATE】讲之前的消费者和提供者注册到Eureka成为高可用,并修改服务调用的硬编码问题,修改为动态获取

--- legend-user/src/main/java/com/legend/user/config/MvcConfiguration.java
+++ legend-user/src/main/java/com/legend/user/config/MvcConfiguration.java
@@ -1,6 +1,9 @@
 package com.legend.user.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -8,4 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private HandlerInterceptor myInterceptor;
+
+    /**
+     * 重写接口中的addInterceptors方法，添加自定义拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+    }
 }
