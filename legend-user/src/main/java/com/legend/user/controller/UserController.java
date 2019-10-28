e0354adec42f4f45c813e9239c20cf7fc7e1874c
qichunlin <737795279@qq.com>
2019-10-28 10:02:29 +0800

    [ADD]增加http-demo服务http客户端案例

--- legend-user/src/main/java/com/legend/user/controller/UserController.java
+++ legend-user/src/main/java/com/legend/user/controller/UserController.java
@@ -3,10 +3,13 @@ package com.legend.user.controller;
 import com.legend.user.pojo.User;
 import com.legend.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -14,6 +17,18 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+
+    @GetMapping("/all")
+    public String all(ModelMap model) {
+        // 查询用户
+        List<User> users = this.userService.queryAll();
+        // 放入模型
+        model.addAttribute("users", users);
+        // 返回模板名称（就是classpath:/templates/目录下的html文件名）
+        return "users";
+    }
+
 
     @GetMapping("{id}")
     public User queryUserById(@PathVariable("id")Long id){
