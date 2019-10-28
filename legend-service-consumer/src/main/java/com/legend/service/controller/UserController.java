53b50572f4c9daba77fad41268cc513506505613
qichunlin <737795279@qq.com>
2019-10-28 19:12:50 +0800

    【UPDATE】讲之前的消费者和提供者注册到Eureka成为高可用,并修改服务调用的硬编码问题,修改为动态获取

--- legend-service-consumer/src/main/java/com/legend/service/controller/UserController.java
+++ legend-service-consumer/src/main/java/com/legend/service/controller/UserController.java
@@ -2,11 +2,15 @@ package com.legend.service.controller;
 
 import com.legend.service.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("consumer/user")
@@ -18,8 +22,18 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    /**
+     * 包含了拉取的所有服务信息
+     */
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping
     public User queryUserById(@RequestParam("id") Long id){
-        return this.restTemplate.getForObject("http://localhost:8099/user/"+id, User.class);
+        //换成动态的服务
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+        ServiceInstance instance = instances.get(0);
+        return this.restTemplate.getForObject("http://"+instance.getHost()+":"+instance.getPort()+"/user/"+id, User.class);
+        //return this.restTemplate.getForObject("http://localhost:8099/user/"+id, User.class);
     }
 }
--- legend-service-provider/src/main/java/com/legend/service/LegendServiceProviderApplication.java
+++ legend-service-provider/src/main/java/com/legend/service/LegendServiceProviderApplication.java
@@ -2,10 +2,12 @@ package com.legend.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.legend.service.mapper") //Mapper接口的包扫描
+@EnableDiscoveryClient
 public class LegendServiceProviderApplication {
 
     public static void main(String[] args) {
