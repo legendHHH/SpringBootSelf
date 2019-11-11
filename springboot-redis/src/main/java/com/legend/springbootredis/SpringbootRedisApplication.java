3d0ca1aa0815739a2e7d690fad5a968d91109fde
qichunlin <737795279@qq.com>
2019-11-11 10:49:16 +0800

    【ADD】 SpringBoot整合Redis缓存-编写RedisTemplate接口测试和相关配置

--- springboot-redis/src/main/java/com/legend/springbootredis/SpringbootRedisApplication.java
+++ springboot-redis/src/main/java/com/legend/springbootredis/SpringbootRedisApplication.java
@@ -1,8 +1,10 @@
 package com.legend.springbootredis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.legend.springbootredis.mapper")
 @SpringBootApplication
 public class SpringbootRedisApplication {
 
