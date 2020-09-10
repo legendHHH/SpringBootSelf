40e2cc43674ef30bb28ade27c91ab576065b7776
qichunlin <737795279@qq.com>
2020-09-10 21:23:47 +0800

    [FIX] 复习springboot整合jdbc并使用jdbcTemplate操作数据库

--- springboot-data-jdbc/src/main/java/com/legend/springboot/controller/HelloController.java
+++ springboot-data-jdbc/src/main/java/com/legend/springboot/controller/HelloController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2020/9/10
+ */
 @Controller
 public class HelloController {
 
@@ -16,9 +22,28 @@ public class HelloController {
     private JdbcTemplate jdbcTemplate;
 
 
+    /**
+     * 插入数据
+     * http://localhost:8080/add?departmentName=legend
+     *
+     * @return
+     */
+    @ResponseBody
+    @GetMapping("/add")
+    public String map(String departmentName) {
+        int result = jdbcTemplate.update("INSERT INTO `springboot`.`department`(`department_name`) VALUES (?)", departmentName);
+        return "添加结果情况：" + result;
+    }
+
+    /**
+     * 查询数据
+     * http://localhost:8080/query
+     *
+     * @return
+     */
     @ResponseBody
     @GetMapping("/query")
-    public Map<String,Object> map(){
+    public Map<String, Object> map() {
         List<Map<String, Object>> list = jdbcTemplate.queryForList("select * FROM department");
         return list.get(0);
     }
