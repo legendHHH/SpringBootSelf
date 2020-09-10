41b7770a88c1104f6ed4da0836b22e2de50e475a
qichunlin <737795279@qq.com>
2020-09-10 22:25:01 +0800

    [FIX] 复习springboot整合PageHelper分页插件

--- springboot-data-mybatis/src/main/java/com/legend/springboot/mapper/EmployeeMapper.java
+++ springboot-data-mybatis/src/main/java/com/legend/springboot/mapper/EmployeeMapper.java
@@ -3,10 +3,14 @@ package com.legend.springboot.mapper;
 
 import com.legend.springboot.bean.Employee;
 
+import java.util.List;
+
 //@Mapper或者@MapperScan将接口扫描装配到容器中
 public interface EmployeeMapper {
 
     public Employee getEmpById(Integer id);
+
+    public List<Employee> selectAll();
 
     public void insertEmp(Employee employee);
 }
