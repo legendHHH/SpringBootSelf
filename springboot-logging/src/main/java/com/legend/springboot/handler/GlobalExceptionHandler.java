1014c39c3250b812e8c58ca2933e53342399355a
qichunlin <737795279@qq.com>
2020-09-12 01:57:45 +0800

    [UPDATE] 全局异常拦截处理

---
+++ springboot-logging/src/main/java/com/legend/springboot/handler/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.legend.springboot.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 全局异常拦截处理
+ *
+ * @author chunlin.qi@hand-china.com
+ * @version 1.0
+ * @description
+ * @date 2020/9/12
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 运行时异常
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseBody
+    public Map<String, String> runtimeException(RuntimeException e) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("code", "500");
+        map.put("message", "运行时异常,正在紧急修复中....");
+        map.put("exception", e.getMessage());
+        System.out.println("hello");
+        return map;
+    }
+
+    /**
+     * 空指针异常
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseBody
+    public Map<String, String> nullPointException(NullPointerException e) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("code", "501");
+        map.put("message", "空指针异常,正在紧急修复中....");
+        map.put("exception", e.getMessage());
+        System.out.println("hello");
+        return map;
+    }
+}
