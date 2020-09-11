24b05884ce616c57f71865f3a950a31bca55e992
qichunlin <737795279@qq.com>
2020-09-12 01:57:45 +0800

    [UPDATE] 全局异常拦截处理

--- springboot-logging/src/main/java/com/legend/springboot/controller/LoggingController.java
+++ springboot-logging/src/main/java/com/legend/springboot/controller/LoggingController.java
@@ -22,9 +22,43 @@ public class LoggingController {
      */
     private static Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
+
+    /**
+     * 全局异常拦截处理(空指针异常)
+     * <p>
+     * http://localhost:8080/exception2
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping("/exception2")
+    @ResponseBody
+    public String exception2(String name) {
+        logger.info("hello");
+        String str = null;
+        str.substring(0, 1);
+        return "log:" + name;
+    }
+
+    /**
+     * 全局异常拦截处理(运行时异常)
+     * <p>
+     * http://localhost:8080/exception
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping("/exception")
+    @ResponseBody
+    public String exception(String name) {
+        logger.info("hello");
+        int i = 10 / 0;
+        return "log:" + name;
+    }
+
     /**
      * AOP切面类处理接口测试
-     *
+     * <p>
      * http://localhost:8080/log?name=legend
      *
      * @param name
