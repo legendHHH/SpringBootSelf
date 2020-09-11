d28e755cad5b26de1425fbab080386ed070987f4
qichunlin <737795279@qq.com>
2020-09-11 10:32:16 +0800

    [UPDATE] 补充springboot整合DataSource多数据源使用JTA+Atomic管理分布式事务问题

--- springboot-datasource/src/main/java/com/qcl/datasource/config/DBConfig2.java
+++ springboot-datasource/src/main/java/com/qcl/datasource/config/DBConfig2.java
@@ -1,14 +1,31 @@
 package com.qcl.datasource.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+/**
+ * 数据库配置类
+ * 如果不要这个类,可以直接在这个类 com.qcl.datasource.config.MyBatisConfig1上添加 @ConfigurationProperties(prefix = "spring.datasource.test1")读取配置
+ *
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2020/9/11
+ */
+//@Component
 @ConfigurationProperties(prefix = "spring.datasource.test2")
 public class DBConfig2 {
     private String url;
-
     private String username;
-
     private String password;
+    private int minPoolSize;
+    private int maxPoolSize;
+    private int maxLifeTime;
+    private int borrowConnectionTimeout;
+    private int loginTimeout;
+    private int maintenanceInterval;
+    private int maxIdleTime;
+    private String testQuery;
 
     public String getUrl() {
         return url;
@@ -32,5 +49,69 @@ public class DBConfig2 {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getMinPoolSize() {
+        return minPoolSize;
+    }
+
+    public void setMinPoolSize(int minPoolSize) {
+        this.minPoolSize = minPoolSize;
+    }
+
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public int getMaxLifeTime() {
+        return maxLifeTime;
+    }
+
+    public void setMaxLifeTime(int maxLifeTime) {
+        this.maxLifeTime = maxLifeTime;
+    }
+
+    public int getBorrowConnectionTimeout() {
+        return borrowConnectionTimeout;
+    }
+
+    public void setBorrowConnectionTimeout(int borrowConnectionTimeout) {
+        this.borrowConnectionTimeout = borrowConnectionTimeout;
+    }
+
+    public int getLoginTimeout() {
+        return loginTimeout;
+    }
+
+    public void setLoginTimeout(int loginTimeout) {
+        this.loginTimeout = loginTimeout;
+    }
+
+    public int getMaintenanceInterval() {
+        return maintenanceInterval;
+    }
+
+    public void setMaintenanceInterval(int maintenanceInterval) {
+        this.maintenanceInterval = maintenanceInterval;
+    }
+
+    public int getMaxIdleTime() {
+        return maxIdleTime;
+    }
+
+    public void setMaxIdleTime(int maxIdleTime) {
+        this.maxIdleTime = maxIdleTime;
+    }
+
+    public String getTestQuery() {
+        return testQuery;
+    }
+
+    public void setTestQuery(String testQuery) {
+        this.testQuery = testQuery;
     }
 }
