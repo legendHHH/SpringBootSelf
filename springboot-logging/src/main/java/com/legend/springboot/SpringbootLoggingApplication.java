e0e4430ff8f0f230c7d2e1071388b81985c6f8c5
qichunlin <737795279@qq.com>
2020-09-12 02:32:38 +0800

    [UPDATE] springboot整合Schedule任务调度定时任务和异步处理Async

--- springboot-logging/src/main/java/com/legend/springboot/SpringbootLoggingApplication.java
+++ springboot-logging/src/main/java/com/legend/springboot/SpringbootLoggingApplication.java
@@ -2,8 +2,21 @@ package com.legend.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 注解EnableScheduling 开启定时任务
+ * 注解EnableAsync 开启异步
+ *
+ * @author chunlin.qi@hand-china.com
+ * @version 1.0
+ * @description
+ * @date 2020/9/12
+ */
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class SpringbootLoggingApplication {
 
     public static void main(String[] args) {
