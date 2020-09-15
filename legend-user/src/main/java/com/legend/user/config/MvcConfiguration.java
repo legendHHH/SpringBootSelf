70f3296d0d611d3f4d747e424f97fc4e67c5c381
qichunlin <737795279@qq.com>
2020-09-15 20:07:11 +0800

    [UPDATE] 修改拦截器

--- legend-user/src/main/java/com/legend/user/config/MvcConfiguration.java
+++ legend-user/src/main/java/com/legend/user/config/MvcConfiguration.java
@@ -1,5 +1,6 @@
 package com.legend.user.config;
 
+import com.legend.user.interceptor.MyInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -8,15 +9,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 定义配置类,注册拦截器
+ * 1.声明该类是一个java配置类
+ * 2.实现WebMvcConfigurer接口
+ *
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2019/10/27
  */
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
     @Autowired
-    private HandlerInterceptor myInterceptor;
+    private MyInterceptor myInterceptor;
 
     /**
      * 重写接口中的addInterceptors方法，添加自定义拦截器
+     *
      * @param registry
      */
     @Override
