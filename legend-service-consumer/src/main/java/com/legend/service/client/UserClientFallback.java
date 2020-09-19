d12a95403d8957acfe279eb55e63621a2f929046
qichunlin <737795279@qq.com>
2020-09-19 18:27:35 +0800

    [UPDATE] openFeign降级

--- legend-service-consumer/src/main/java/com/legend/service/client/UserClientFallback.java
+++ legend-service-consumer/src/main/java/com/legend/service/client/UserClientFallback.java
@@ -1,4 +1,23 @@
 package com.legend.service.client;
 
-public class UserClientFallback {
+import com.legend.service.pojo.User;
+import org.springframework.stereotype.Component;
+
+/**
+ * Feign降级类
+ *
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2019/11/2
+ */
+@Component
+public class UserClientFallback implements UserClient {
+
+    @Override
+    public User queryById(Long id) {
+        User user = new User();
+        user.setUserName("服务器繁忙，请稍后再试！");
+        return user;
+    }
 }
