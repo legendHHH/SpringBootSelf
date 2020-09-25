bb59ab6b35a995c84fd73691acc814076429658f
qichunlin <737795279@qq.com>
2020-09-25 23:23:32 +0800

    [UPDATE] permission权限项目全局异常类定义以及返回json

---
+++ springmvc-permission/src/main/java/com/qcl/permission/exception/ParamException.java
@@ -0,0 +1,31 @@
+package com.qcl.permission.exception;
+
+/**
+ * 自定义参数异常类
+ *
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2020/9/24
+ */
+public class ParamException extends RuntimeException {
+    public ParamException() {
+        super();
+    }
+
+    public ParamException(String message) {
+        super(message);
+    }
+
+    public ParamException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ParamException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ParamException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
