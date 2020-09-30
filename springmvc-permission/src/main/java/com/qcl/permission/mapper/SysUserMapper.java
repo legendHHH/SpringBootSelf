ecd22f9e3457805e57b20c898cc03b3dbb900529
qichunlin <737795279@qq.com>
2020-09-30 15:04:28 +0800

    ‘’

--- springmvc-permission/src/main/java/com/qcl/permission/mapper/SysUserMapper.java
+++ springmvc-permission/src/main/java/com/qcl/permission/mapper/SysUserMapper.java
@@ -1,6 +1,9 @@
 package com.qcl.permission.mapper;
 
 import com.qcl.permission.model.SysUser;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * mapper
@@ -11,15 +14,96 @@ import com.qcl.permission.model.SysUser;
  * @date 2020/9/25
  */
 public interface SysUserMapper {
-    int deleteByPrimaryKey(Integer id);
+    /**
+     * 删除根据主键
+     *
+     * @param id
+     * @return
+     */
+    int deleteByPrimaryKey(@Param("id") Integer id);
 
+    /**
+     * 插入用户
+     *
+     * @param record
+     * @return
+     */
     int insert(SysUser record);
 
+    /**
+     * 有选择性插入用户
+     *
+     * @param record
+     * @return
+     */
     int insertSelective(SysUser record);
 
-    SysUser selectByPrimaryKey(Integer id);
+    /**
+     * 根据主键查询
+     *
+     * @param id
+     * @return
+     */
+    SysUser selectByPrimaryKey(@Param("id") Integer id);
 
+    /**
+     * 根据主键有选择性更新数据
+     *
+     * @param record
+     * @return
+     */
     int updateByPrimaryKeySelective(SysUser record);
 
+    /**
+     * 更新根据主键
+     *
+     * @param record
+     * @return
+     */
     int updateByPrimaryKey(SysUser record);
+
+    /**
+     * 查询用户
+     *
+     * @param keyword
+     * @return
+     */
+    SysUser findByKeyword(@Param("keyword") String keyword);
+
+    /**
+     * 校验邮箱
+     *
+     * @param mail
+     * @param id
+     * @return
+     */
+    int countByMail(@Param("mail") String mail, @Param("id") Integer id);
+
+    /**
+     * 校验电话
+     *
+     * @param telephone
+     * @param id
+     * @return
+     */
+    int countByTelephone(@Param("telephone") String telephone, @Param("id") Integer id);
+
+    /**
+     * 校验部门
+     *
+     * @param deptId
+     * @return
+     */
+    int countByDeptId(@Param("deptId") int deptId);
+
+    List<SysUser> getPageByDeptId(@Param("deptId") int deptId, @Param("page") PageQuery page);
+
+    List<SysUser> getByIdList(@Param("idList") List<Integer> idList);
+
+    /**
+     * 获取所有用户
+     *
+     * @return
+     */
+    List<SysUser> getAll();
 }
