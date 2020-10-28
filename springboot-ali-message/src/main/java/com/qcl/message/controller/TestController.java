0e7b9538a4b2485af8ca454dad774fa11b35fd22
qichunlin <737795279@qq.com>
2020-10-28 08:59:00 +0800

    ‘’

--- springboot-ali-message/src/main/java/com/qcl/message/controller/TestController.java
+++ springboot-ali-message/src/main/java/com/qcl/message/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.qcl.message.controller;
 
+import com.qcl.message.config.AliyunSmsSendProperty;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author chunlin.qi@hand-china.com
@@ -29,5 +33,20 @@ public class TestController {
     public String downloadLog2(HttpServletResponse response, @PathVariable("date") String date) {
         System.out.println(date);
         return "true:/hbrm/log/download/{date}";
+    }
+
+    public static void main(String[] args) {
+        List<AliyunSmsSendProperty> aliyunSmsSendPropertyArrayList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            AliyunSmsSendProperty aliyunSmsSendProperty = new AliyunSmsSendProperty();
+            aliyunSmsSendProperty.setAccessKeyId("66"+ i);
+            aliyunSmsSendPropertyArrayList.add(aliyunSmsSendProperty);
+        }
+        aliyunSmsSendPropertyArrayList.forEach(ali ->{
+            ali.setSingleName("singleName");
+        });
+
+        System.out.println(aliyunSmsSendPropertyArrayList.toString());
+
     }
 }
