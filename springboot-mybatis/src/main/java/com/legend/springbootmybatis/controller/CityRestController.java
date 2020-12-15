395f5786fbf3f1855ad9192bbd6a118af5985548
qichunlin <737795279@qq.com>
2020-12-15 16:20:50 +0800

    [UPDATE] 保存或更新/批量保存或更新

--- springboot-mybatis/src/main/java/com/legend/springbootmybatis/controller/CityRestController.java
+++ springboot-mybatis/src/main/java/com/legend/springbootmybatis/controller/CityRestController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class CityRestController {
@@ -20,4 +23,24 @@ public class CityRestController {
         return cityService.findCityByName(cityName);
     }
 
+
+    @RequestMapping(value = "/api/city/saveOrUpdate", method = RequestMethod.GET)
+    public int saveOrUpdate(City city) {
+        return cityService.saveOrUpdate(city);
+    }
+
+
+    @RequestMapping(value = "/api/city/batchSaveOrUpdate", method = RequestMethod.GET)
+    public int batchSaveOrUpdate() {
+        List<City> cityList = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            City city = new City();
+            city.setDescription("北京路" + i + i);
+            city.setCityName("北京路" + i);
+            city.setProvinceId(i + 10000L);
+            city.setId(i + 10L);
+            cityList.add(city);
+        }
+        return cityService.batchSaveOrUpdate(cityList);
+    }
 }
