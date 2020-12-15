c0da6624dd9a30232b12170af9fb07aab6b8f6bb
qichunlin <737795279@qq.com>
2020-12-15 15:27:06 +0800

    ''

--- springboot-test/src/main/java/com/legend/demo/test2/controller/HelloController2.java
+++ springboot-test/src/main/java/com/legend/demo/test2/controller/HelloController2.java
@@ -1,6 +1,8 @@
 package com.legend.demo.test2.controller;
 
+import com.legend.demo.test2.domain.User;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,5 +13,12 @@ public class HelloController2 {
     @RequestMapping("/show2")
     public String helloW() {
         return "HAND Hello";
+    }
+
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String helloUser(User user) {
+        System.out.println(user.toString());
+        return "HAND Hello" + user;
     }
 }
---
+++ springboot-test/src/main/java/com/legend/demo/test2/domain/User.java
@@ -0,0 +1,37 @@
+package com.legend.demo.test2.domain;
+
+/**
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2020/12/15
+ */
+public class User {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
