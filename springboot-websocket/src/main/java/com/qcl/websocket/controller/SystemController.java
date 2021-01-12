7ccbe2acb5b877ab2acf60a0e7cf4e0741a7e2e8
qichunlin <737795279@qq.com>
2021-01-12 21:13:41 +0800

    [UPDATE] WebSocket实现前后端推送

---
+++ springboot-websocket/src/main/java/com/qcl/websocket/controller/SystemController.java
@@ -0,0 +1,58 @@
+package com.qcl.websocket.controller;
+
+import com.qcl.websocket.ws.WebSocketServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2021/1/12
+ */
+@Controller("web_Scoket_system")
+@RequestMapping("/api/socket")
+public class SystemController {
+
+    /**
+     * 页面请求
+     *
+     * @param userId
+     * @return
+     */
+    @GetMapping("/index/{userId}")
+    public ModelAndView socket(@PathVariable String userId) {
+        ModelAndView mav = new ModelAndView("/socket1");
+        mav.addObject("userId", userId);
+        return mav;
+    }
+
+    /**
+     * 推送数据接口
+     *
+     * @param cid
+     * @param message
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/socket/push/{cid}")
+    public Map pushToWeb(@PathVariable String cid, String message) {
+        Map<String, Object> result = new HashMap<>();
+        try {
+            WebSocketServer.sendInfo(message, cid);
+            result.put("code", cid);
+            result.put("msg", message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
