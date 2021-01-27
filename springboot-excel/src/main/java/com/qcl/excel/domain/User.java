6c795a9f734d3ba107f3438ef31f32909eace819
qichunlin <737795279@qq.com>
2021-01-27 15:32:15 +0800

    [UPDATE] 使用EasyExcel工具快速导出Excel

--- springboot-excel/src/main/java/com/qcl/excel/domain/User.java
+++ springboot-excel/src/main/java/com/qcl/excel/domain/User.java
@@ -2,19 +2,31 @@ package com.qcl.excel.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.Date;
 
 /**
- * @author chunlin.qi@hand-china.com
+ * t_user表
+ *
+ * @author legend
  * @version 1.0
  * @description
  * @date 2021/1/22
  */
 @Data
+@Entity //告诉JPA这是一个实体类（和数据表映射的类）
+@Table(name = "t_user") //@Table来指定和哪个数据表对应;如果省略默认表名就是user;
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String name;
     private String address;
     private Date birth;
+    private String sex;
+    private String image;
+    private String remark;
 
     public User() {
     }
