a1cfd171ac8390e259ac5b820bd3dbdb5b495f6f
qichunlin <737795279@qq.com>
2021-03-02 17:18:41 +0800

    [UPDATE] mysql数据库密码整合druid加密解密

--- springboot-data-jdbc/src/main/java/com/legend/springboot/controller/HelloController.java
+++ springboot-data-jdbc/src/main/java/com/legend/springboot/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.legend.springboot.controller;
 
+import com.alibaba.druid.filter.config.ConfigTools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -47,4 +48,18 @@ public class HelloController {
         List<Map<String, Object>> list = jdbcTemplate.queryForList("select * FROM department");
         return list.get(0);
     }
+
+    public static void main(String[] args) throws Exception {
+        // 需要加密的明文命名
+        String password = "123456"; // 【注意：这里要改为你自己的密码】
+        // 调用 druid 生成私钥、公钥、密文
+        ConfigTools.main(new String[]{password});
+
+        String privateKey = "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAoa5rY4eJMpQHj60Fua4qWrueHP4PGHsixWJorJfTO0Sh3mIBh14dFKtG6bA9OSTEtaKOQR232oBICbgG1kTCbwIDAQABAkAyJk/ZB2le7tuJjTmBZqxsiFV62LMF/OvEHcop/s3eS+RIy7ocEB5sXImRFLCFBXjXY41BjmuQU2BnaiFMv3zBAiEA51RVYcc1+mYz2Bf8lBO0mjeZB8eC5VdqW+ZJbNgLSGsCIQCy7JSYzTnV9MBMshbGvpmZqBgtDCV8yqE/UiWms49/DQIgdbBkF/C/t3rTmXbqm3NDCMqMYnbQU1mb1NFePa/UXa0CIHmT8FTz/ry3loOG75sUoGKcqfDSNBbbyT2xoAvqLzNxAiEAvwGDJdoshsULOA+LP4ezLonY7mYwFvKjHcZWjqPRhMI=";
+        String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKGua2OHiTKUB4+tBbmuKlq7nhz+Dxh7IsViaKyX0ztEod5iAYdeHRSrRumwPTkkxLWijkEdt9qASAm4BtZEwm8CAwEAAQ==";
+        String password2 = "AvY2gZrjNmQjJIqlir3VSQib1KO3TIrKT3V9oNKgOIzS6c9cUR1ZLOMsoscf2XylZt6qauZFXbstHRqXkb0QWg==";
+        String decrypt = ConfigTools.decrypt(publicKey, password2);
+        System.out.println(decrypt);
+    }
+
 }
