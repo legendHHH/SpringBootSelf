869541be29808ce43eff4ca5c9e8e05587ed7c19
qichunlin <737795279@qq.com>
2021-04-28 17:22:24 +0800

    [UPDATE] springboot整合Dataway

---
+++ springboot-dataway/src/main/java/com/legend/dataway/springbootdataway/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.legend.dataway.springbootdataway.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 控制器
+ *
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2021/4/25
+ */
+@RestController
+public class TestController {
+}
---
+++ springboot-dataway/src/test/java/com/legend/dataway/springbootdataway/SpringbootDatawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legend.dataway.springbootdataway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
