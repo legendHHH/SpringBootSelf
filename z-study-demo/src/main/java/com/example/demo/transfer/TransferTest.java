7d3b80a78a3df01a8eba2d112cad2156c1f14697
qichunlin <737795279@qq.com>
2021-06-06 10:55:54 +0800

    [UPDATE] 交换两个值不使用临时变量换成异或运算解决

---
+++ z-study-demo/src/main/java/com/example/demo/transfer/TransferTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.transfer;
+
+/**
+ * 异不使用新的变量，交换两个变量的值(异或运算测试)
+ *
+ * @author legend
+ * @version 1.0
+ * @description
+ * @date 2021/6/6
+ */
+public class TransferTest {
+
+    public static void main(String[] args) {
+        //不使用新的变量，交换两个变量的值
+        int a = 10;
+        int b = 12;
+
+        //异或运算：相同为0，不同为1
+        //1010  1100 得到的是一个中间值
+        a = a ^ b;
+
+        //真正开始进行交换
+        b = a ^ b;
+        a = a ^ b;
+
+        //a=12,b=10
+        System.out.println("a===>" + a);
+        System.out.println("b===>" + b);
+    }
+}
