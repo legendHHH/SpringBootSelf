f8f26d5b56cbd9a29977441098cd2d0b212dbd31
qichunlin <737795279@qq.com>
2021-06-12 23:26:21 +0800

    ''

--- z-study-demo/src/main/java/com/example/demo/equals/TestEquals.java
+++ z-study-demo/src/main/java/com/example/demo/equals/TestEquals.java
@@ -2,15 +2,24 @@ package com.example.demo.equals;
 
 import com.example.demo.string.Person;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 1. == 和 equals
  *
- * == 比较基本引用类型+基本类型
+ * ==
+ *    比较基本引用类型+基本类型
  *    引用类型比较的是内存地址：new
  *      基本类型：比较值
+ *
+ * equals
+ *      默认Object类 就是 ==
+ *      复写，具体情况具体分析
+ *
  * @author legend
  * @version 1.0
  * @description
@@ -23,7 +32,12 @@ public class TestEquals {
         String s2 = new String("abc");
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
+
+        //HashSet是线程不安全的
         Set<String> strSet = new HashSet<>();
+
+        //可以包装成线程安全的
+        //Collections.synchronizedSet();
         strSet.add(s1);
         strSet.add(s2);
         System.out.println(strSet.size());
@@ -40,5 +54,9 @@ public class TestEquals {
         set.add(p2);
         System.out.println(p1.hashCode() +"\t" + p2.hashCode());
         System.out.println(set.size());
+
+        //复习源码
+        new HashMap<>(16).put(1,1);
+        new ConcurrentHashMap<>(16).put(1,1);
     }
 }
--- z-study-demo/src/main/java/com/example/demo/string/Person.java
+++ z-study-demo/src/main/java/com/example/demo/string/Person.java
@@ -1,14 +1,23 @@
 package com.example.demo.string;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
 
 /**
+ * Person类不能直接用@Data注解,这个注解默认帮我们重写了hashCode,如果我们需要比较两个对象是否相等会返回true
+ *
  * @author legend
  * @version 1.0
  * @description
  * @date 2021/3/17
  */
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
 public class Person {
     private Integer id;
     private String personName;
@@ -19,4 +28,23 @@ public class Person {
     public Person(String personName) {
         this.personName = personName;
     }
+
+    /**
+     * 如果名字相同就是同一个对象,可以使用下面的方法
+     *
+     * @param o
+     * @return
+     */
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(personName, person.personName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(personName);
+    }*/
 }
