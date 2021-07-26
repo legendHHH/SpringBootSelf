d689d1c874354dd58ef20d6d1b5b850a41d283ae
qichunlin <737795279@qq.com>
2021-07-27 05:31:58 +0800

    [UPDATE] redis使用lua脚本测试

--- springboot-redis/src/main/java/com/legend/springbootredis/SpringbootRedisApplication.java
+++ springboot-redis/src/main/java/com/legend/springbootredis/SpringbootRedisApplication.java
@@ -3,6 +3,10 @@ package com.legend.springbootredis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
 
 @MapperScan("com.legend.springbootredis.mapper")
 @SpringBootApplication
@@ -12,4 +16,11 @@ public class SpringbootRedisApplication {
         SpringApplication.run(SpringbootRedisApplication.class, args);
     }
 
+    @Bean
+    public DefaultRedisScript<Boolean> redisScript() {
+        DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<>();
+        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("script/Test.lua")));
+        redisScript.setResultType(Boolean.class);
+        return redisScript;
+    }
 }
--- springboot-redis/src/main/java/com/legend/springbootredis/controller/RedisTestController.java
+++ springboot-redis/src/main/java/com/legend/springbootredis/controller/RedisTestController.java
@@ -1,11 +1,14 @@
 package com.legend.springbootredis.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * redis测试接口
@@ -18,8 +21,12 @@ public class RedisTestController {
     @Autowired
     StringRedisTemplate redisTemplate;
 
+    @Autowired
+    private DefaultRedisScript<Boolean> redisScript;
+
     /**
      * 测试key value
+     * http://localhost:8081/redisTest/set/kp/999
      *
      * @param key
      * @param value
@@ -31,9 +38,50 @@ public class RedisTestController {
         return key + ":" + value;
     }
 
+    /**
+     * 获取key的值
+     * http://localhost:8081/redisTest/get/kp
+     *
+     * @param key
+     * @return
+     */
     @RequestMapping(value = "get/{key}", method = RequestMethod.GET)
     public String testGet(@PathVariable String key) {
         String value = redisTemplate.opsForValue().get(key);
         return "根据key获取的value是" + value;
+    }
+
+    /**
+     * 测试设置xxxx
+     *
+     * @param request
+     * @param value
+     * @return
+     */
+    @RequestMapping(value = "set/{value}", method = RequestMethod.GET)
+    @ResponseBody
+    public String test(HttpServletRequest request, @PathVariable String value) {
+        Thread thread = Thread.currentThread();
+        System.out.println("当前请求：" + request.getSession().getId());
+        System.out.println("当前线程：" + thread);
+        String key = thread.getId() + "";
+        //redisTemplate.opsForValue().set(key, value);
+        Thread.currentThread().getPriority();
+        return key + ":" + value;
+    }
+
+    /**
+     * 测试Lua操作redis
+     *
+     * @return
+     */
+    @GetMapping("/lua")
+    @ResponseBody
+    public String testLua() {
+        List<String> keys = Arrays.asList("testLua", "hello lua");
+        System.out.println("script字符串："+redisScript.getScriptAsString());
+        Boolean execute = redisTemplate.execute(redisScript, keys, "100");
+        System.out.println(execute);
+        return "abc";
     }
 }
