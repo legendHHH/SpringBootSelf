367ee793bbd31ed119ca21614064bcb2328c1fa9
qichunlin <737795279@qq.com>
2021-08-28 23:48:36 +0800

    [UPDATE] python导入excel数据到mysql数据库

---
+++ python-study/mysql.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import pymysql
+
+/**
+创建链接
+conn = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='123456',
+    port=3306,
+    db='test',
+    charset='utf8'
+)
+
+
+df = pd.read_excel("C:\\Users\\legend\\Desktop\\test.xlsx")
+for idx,row in df.iterrows():
+    sql= f"""\
+        INSERT INTO `test`(`one`, `two`, `createTime`) VALUES ('{row.one}', '{row.two}', '{row.createTime}');
+"""
+
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    
+    conn.commit()
+    
+conn.close()
