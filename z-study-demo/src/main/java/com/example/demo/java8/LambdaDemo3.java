98181308943d44d0adfd8fe8729ce7f59f719fcf
qichunlin <737795279@qq.com>
2021-11-06 21:54:42 +0800

    [UPDATE] 在lambda中实现continue的效果

--- z-study-demo/src/main/java/com/example/demo/java8/LambdaDemo3.java
+++ z-study-demo/src/main/java/com/example/demo/java8/LambdaDemo3.java
@@ -20,5 +20,20 @@ public class LambdaDemo3 {
         for (Dish apple : appleList) {
             apple.setCalories((int) (5.0 * apple.getCalories() / 1000));
         }
+
+        //在lambda中实现continue的效果
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.forEach(i -> {
+            if (i.intValue() == 1) {
+                return;
+            }
+
+            System.out.println(i);
+            System.out.println(123);
+        });
+        System.out.println("haha");
     }
 }
