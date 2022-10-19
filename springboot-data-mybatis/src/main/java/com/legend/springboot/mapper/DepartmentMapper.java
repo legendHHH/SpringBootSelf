ad0f8e2b051adaa31a1dd09c99aa3b29297cb1a7
qichunlin <737795279@qq.com>
2022-10-19 18:02:48 +0800

    [UPDATE]MyBatis基于TypeHandler配置实现加密、解密

--- springboot-data-mybatis/src/main/java/com/legend/springboot/mapper/DepartmentMapper.java
+++ springboot-data-mybatis/src/main/java/com/legend/springboot/mapper/DepartmentMapper.java
@@ -2,6 +2,7 @@ package com.legend.springboot.mapper;
 
 
 import com.legend.springboot.bean.Department;
+import com.legend.springboot.myencrypt.Encrypt;
 import org.apache.ibatis.annotations.*;
 
 /**
@@ -20,6 +21,13 @@ public interface DepartmentMapper {
     @Options(useGeneratedKeys = true,keyProperty = "id")
     @Insert("insert into department(department_name) values(#{departmentName}) ")
     public int insertDept(Department departmentName);
+
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    @Insert("insert into department(department_name, dept_name_encrypt) values(#{departmentName},#{deptNameEncrypt}) ")
+    public int insertDeptEncrypt(Department departmentName);
+
+    //@Select("select * from department where dept_name_encrypt=#{deptNameEncrypt}")
+    public Department getDeptByIdDecrypt(@Param("id") Integer id, @Param("deptNameEncrypt") Encrypt deptNameEncrypt);
 
     @Update("update department set department_name=#{deptmentName} where id=#{id}")
     public Department updateDept(Department department);
