18a4905d6e106943d94f72faff3a4f2720a337f4
qichunlin <737795279@qq.com>
2023-02-23 22:43:15 +0800

    [UPDATE]redis的GEO

--- springboot-redis/src/main/java/com/legend/springbootredis/controller/UserController.java
+++ springboot-redis/src/main/java/com/legend/springbootredis/controller/UserController.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.legend.springbootredis.entity.User;
 import com.legend.springbootredis.service.RedisService;
+import org.redisson.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -58,6 +60,43 @@ public class UserController {
         System.out.println(" value : "+value);
         modelMap1.put("从缓存中根据key取到的value", value);
         return modelMap1;
+    }
+
+
+    /**
+     * 基于Redis GEO找到离你最近的店铺信息
+     *
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @GetMapping("/redisgeo.do")
+    @ResponseBody
+    public String redisGeoTest() throws ExecutionException, InterruptedException {
+        RedissonClient redissonClient = redisService.getRedissonClient();
+        RGeo<String> geo = redissonClient.getGeo("message:shop");
+
+        //初始化三家店铺信息
+        GeoEntry shopA = new GeoEntry(111.56, 27.51, "shopA");
+        GeoEntry shopB = new GeoEntry(112.54, 26.51, "shopB");
+        GeoEntry shopC = new GeoEntry(113.56, 25.51, "shopC");
+
+        //添加店铺
+        geo.add(shopA, shopB, shopC);
+
+        //获取店铺信息
+        Map<String, GeoPosition> pos = geo.pos("shopA", "shopB", "shopC");
+        System.out.println("A店的位置：" + pos.get("shopA"));
+        System.out.println("B店的位置：" + pos.get("shopB"));
+        System.out.println("C店的位置：" + pos.get("shopC"));
+
+
+        //计算距离 假设 停车场有个经纬度，111.89  26.13 找到最近的店铺  有哪些店
+        Map<String, Double> stringDoubleMap = geo.radiusWithDistanceAsync(111.53, 26.13, 200, GeoUnit.KILOMETERS, GeoOrder.ASC).get();
+        for (String key : stringDoubleMap.keySet()) {
+            System.out.println(key + "离你的距离为：" + stringDoubleMap.get(key));
+        }
+        return null;
     }
 
 }
--- springboot-redis/src/main/java/com/legend/springbootredis/service/RedisService.java
+++ springboot-redis/src/main/java/com/legend/springbootredis/service/RedisService.java
@@ -5,6 +5,10 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.config.Config;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
@@ -14,6 +18,16 @@ import org.springframework.stereotype.Service;
 public class RedisService {
     @Resource
     private StringRedisTemplate template;
+
+
+    public RedissonClient getRedissonClient() {
+        //单机
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.setCodec(new StringCodec());
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
 
     /**
      * 存储数据或修改数据
